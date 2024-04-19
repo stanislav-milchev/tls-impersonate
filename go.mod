6ac50909f4d0e314c03b2d6f29e67b74d6836e8d
Stanislav Milchev <stanislav.milchev@edited.com>
2024-04-19 21:36:48 +0300

    feat: add basic request inside the endpoint The basic http/fhttp request added as a start, working towards the impersonating implementations

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/stanislav-milchev/tls-impersonator
 
 go 1.22.2
+
+require (
+	github.com/Carcraftz/fhttp v0.0.0-20220112175241-f36cd20af880 // indirect
+	github.com/Carcraftz/utls v0.0.0-20220413235215-6b7c52fd78b6 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	gitlab.com/yawning/bsaes.git v0.0.0-20190805113838-0a714cd429ec // indirect
+	gitlab.com/yawning/utls.git v0.0.12-1 // indirect
+	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c // indirect
+	golang.org/x/net v0.0.0-20210610132358-84b48f89b13b // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+
+	http "github.com/Carcraftz/fhttp"
+)
 
 func main() {
-	fmt.Println("Showtime!")
+	port := ":42069"
+	log.Printf("Listening on port %s", port)
+	http.HandleFunc("/", HandleReq)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalln("Error starting the HTTP server:", err)
+	}
+}
+
+// HandleReq takes the incoming request, parses it, sends it towards the
+func HandleReq(w http.ResponseWriter, r *http.Request) {
+	url := r.Header.Get("tls-request")
+	method := r.Method
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	contentType := res.Header.Get("content-type")
+	contentEncoding := res.Header.Get("content-encoding")
+
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Content-Encoding", contentEncoding)
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	w.Write(body)
+
 }
