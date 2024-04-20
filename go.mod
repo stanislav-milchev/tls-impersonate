427306614d6186cac80a1b222f01c0174a78234e
Stanislav Milchev <stanislav.milchev@edited.com>
2024-04-20 18:38:27 +0300

    feat: impersonate session, header order, request parsing AzureTls-Client provided impersonation with currently hardcoded headers only for single browser. TODO: Update the headers, fingerprints and HTTP2 frames to properly impersonate and separate the browsers in a different file

--- go.mod
+++ go.mod
@@ -3,26 +3,25 @@ module github.com/stanislav-milchev/tls-impersonator
 go 1.22.2
 
 require (
-	github.com/CUCyber/ja3transport v0.0.0-20201031204932-8a22ac8ab5d7
-	github.com/Carcraftz/fhttp v0.0.0-20220112175241-f36cd20af880
+	github.com/Danny-Dasilva/fhttp v0.0.0-20240217042913-eeeb0b347ce1
+	github.com/Noooste/azuretls-client v1.4.10
+	github.com/davidmytton/url-verifier v1.0.1
 )
 
 require (
-	github.com/Carcraftz/utls v0.0.0-20220413235215-6b7c52fd78b6 // indirect
-	github.com/Danny-Dasilva/CycleTLS/cycletls v1.0.26 // indirect
-	github.com/Danny-Dasilva/fhttp v0.0.0-20240217042913-eeeb0b347ce1 // indirect
+	github.com/Noooste/fhttp v1.0.12 // indirect
+	github.com/Noooste/utls v1.2.9 // indirect
+	github.com/Noooste/websocket v1.0.3 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
-	github.com/dsnet/compress v0.0.1 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/klauspost/compress v1.17.6 // indirect
-	github.com/quic-go/quic-go v0.41.0 // indirect
-	github.com/refraction-networking/utls v1.6.2 // indirect
-	gitlab.com/yawning/bsaes.git v0.0.0-20190805113838-0a714cd429ec // indirect
-	gitlab.com/yawning/utls.git v0.0.12-1 // indirect
-	golang.org/x/crypto v0.19.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/refraction-networking/utls v1.6.4 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	h12.io/socks v1.0.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
-    fhttp "github.com/Danny-Dasilva/fhttp"
-	"github.com/Danny-Dasilva/CycleTLS/cycletls"
-	"io"
+	"errors"
 	"log"
+	"strconv"
+	"time"
+
+	fhttp "github.com/Danny-Dasilva/fhttp"
+	"github.com/Noooste/azuretls-client"
+	urlverifier "github.com/davidmytton/url-verifier"
 )
+
+var session *azuretls.Session
+var verifier *urlverifier.Verifier
+
+func init() {
+	session = azuretls.NewSession()
+	verifier = urlverifier.NewVerifier()
+}
 
 func main() {
 	port := ":42069"
-	log.Printf("Listening on port %s", port)
+	log.Printf("Listening on localhost%s", port)
 	fhttp.HandleFunc("/", HandleReq)
-
 	err := fhttp.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatalln("Error starting the HTTP server:", err)
@@ -21,44 +31,83 @@ func main() {
 
 // HandleReq takes the incoming request, parses it, sends it towards the
 func HandleReq(w fhttp.ResponseWriter, r *fhttp.Request) {
-	url := r.Header.Get("tls-request")
-	method := r.Method
-
-	req, err := fhttp.NewRequest(method, url, nil)
+	req, err := NewRequest(r)
 	if err != nil {
-		fmt.Println(err)
+		log.Print(err)
+		w.WriteHeader(fhttp.StatusBadRequest)
 		return
 	}
+	/*
+		ja3 := "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-17513-18-16-10-23-13-65281-43-5-51-11-65037-27-35-45,29-23-24,0"
+		ua := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0"
+	*/
 
-	ja3 := "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-17513-18-16-10-23-13-65281-43-5-51-11-65037-27-35-45,29-23-24,0"
-	ua := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0"
-
-	client := &fhttp.Client{
-		Transport: cycletls.NewTransport(ja3, ua),
+	// TODO: update default browser headers and put them in a separate package
+	session.OrderedHeaders = azuretls.OrderedHeaders{
+		{"accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+		{"accept-encoding", "gzip, deflate, br"},
+		{"accept-language", "zh-HK,zh-TW;q=0.9,zh;q=0.8"},
+		{"cache-control", "max-age=0"},
+		{"sec-ch-ua", `"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"`},
+		{"sec-ch-ua-mobile", "?0"},
+		{"sec-ch-ua-platform", `"Windows"`},
+		{"sec-fetch-site", "none"},
+		{"sec-fetch-mode", "navigate"},
+		{"sec-fetch-user", "?1"},
+		{"sec-fetch-dest", "document"},
+		{"user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"},
+		{"upgrade-insecure-requests", "1"},
 	}
 
-	res, err := client.Do(req)
+	res, err := session.Do(req)
 
-	if err != nil {
-		fmt.Println(err)
+	if err != nil && err.Error() == "timeout" {
+		w.WriteHeader(fhttp.StatusRequestTimeout)
+		return
+	} else if err != nil {
+		// TODO: EOF error encountered here at one point. Doesn't seem to happen now.
+		// Potentially could be 'Connection' header issue
+		w.WriteHeader(res.StatusCode)
+		w.Write(res.Body)
 		return
 	}
-
-	defer res.Body.Close()
 
 	contentType := res.Header.Get("content-type")
-	contentEncoding := res.Header.Get("content-encoding")
+	// contentEncoding := res.Header.Get("content-encoding")
 
 	w.Header().Set("Content-Type", contentType)
-	w.Header().Set("Content-Encoding", contentEncoding)
+	// w.Header().Set("Content-Encoding", contentEncoding)
 
-	body, err := io.ReadAll(res.Body)
+	w.WriteHeader(res.StatusCode)
+	w.Write(res.Body)
+}
 
-	if err != nil {
-		fmt.Println(err)
-		return
+func NewRequest(r *fhttp.Request) (*azuretls.Request, error) {
+	// Parse and validate request URL
+	url := r.Header.Get("x-tls-url")
+	res, err := verifier.Verify(url)
+	if err != nil || res.IsURL == false {
+		return nil, errors.New("No valid request URL supplied via 'x-tls-url'; skipping request")
 	}
 
-	w.Write(body)
+	timeout := r.Header.Get("x-tls-timeout")
+	// proxy := r.Header.Get("x-tls-proxy")
+	// redirects := r.Header.Get("x-tls-redirects")
+	method := r.Method
 
+	// Parse timeout
+	t, err := strconv.Atoi(timeout)
+	if err != nil {
+		// Probably dont log that on every request? Do it once and disable a flag or sth
+		log.Println("Invalid timeout value supplied, defaulting to 30s.")
+		t = 30
+	}
+
+	req := azuretls.Request{
+		Method:           method,
+		Url:              url,
+		TimeOut:          time.Duration(t) * time.Second,
+		DisableRedirects: true,
+	}
+	return &req, nil
 }
